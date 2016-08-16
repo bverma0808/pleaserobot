44b9c478887c51ad46f3308c522d695aee2d7a86
Bharat <bharat@silverpush.co>
2016-08-16 20:06:26 +0530

    restify added to make it as a simple web app

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ const TelegramBaseController = Telegram.TelegramBaseController
 const config = require('./config')
 const tg = new Telegram.Telegram(config.BOT_TOKEN)
 
+var messages = '';
+
+var restify = require('restify')
+var server = restify.createServer({
+    name: 'Telegram-Bot-Listener',
+    version: '1.0.0'
+});
+
+// Necessary configuration for restify server
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
 
 class DataController extends TelegramBaseController {
     handle() {
@@ -12,10 +24,25 @@ class DataController extends TelegramBaseController {
     }
 
     before(command, scope) {
-        console.log(`${new Date()}; Received Message:=> `, scope._update._message._text.substring(1));
+        // console.log(`${new Date()}; Received Message:=> `, scope._update._message._text.substring(1));
+        messages += `${new Date()}; Received Message:=> ` + scope._update._message._text.substring(1) + '<br>'
         return scope
     }
 }
 
-tg.router
-    .otherwise(new DataController());
+tg.router.otherwise(new DataController());
+
+
+server.get('/getChatUpdates', function (req, res, next) {
+    res.writeHead(200, {
+      'Content-Length': Buffer.byteLength(messages),
+      'Content-Type': 'text/html'
+    });
+    res.write(messages);
+    res.end();
+});
+
+
+server.listen(7070, function () {
+    console.log('request' ,'%s listening at %s', server.name, server.url);
+});
