60a82b148bca6760a705a934b743a284225a43d8
Bharat <bharat@silverpush.co>
2016-08-16 19:25:40 +0530

    structure changes

--- main.js
+++ main.js
@@ -1,36 +1,21 @@
 'use strict'
-const config = require('./config')
+
 const Telegram = require('telegram-node-bot')
 const TelegramBaseController = Telegram.TelegramBaseController
+const config = require('./config')
 const tg = new Telegram.Telegram(config.BOT_TOKEN)
 
-class PingController extends TelegramBaseController {
-    /**
-     * @param {Scope} $
-     */
-    pingHandler($) {
-        $.sendMessage('pong')
-    }
 
-    get routes() {
-        return {
-            'ping': 'pingHandler'
-        }
-    }
-}
-
-class OtherwiseController extends TelegramBaseController {
+class DataController extends TelegramBaseController {
     handle() {
         // console.log('otherwise')
     }
 
     before(command, scope) {
-        // scope.someData = true
         console.log(`${new Date()}; Received Message:=> `, scope._update._message._text.substring(1));
         return scope
     }
 }
 
 tg.router
-    // .when(['ping'], new PingController())
-    .otherwise(new OtherwiseController());
+    .otherwise(new DataController());
